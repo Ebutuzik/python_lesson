2c21e567b5c29ac4a0cb1b9a769cb28df9b830fa
Aleksandr Chernov <ebutuzik@gmail.com>
2026-01-22 10:34:06 +0700

    python операции

--- lesson_python.py
+++ lesson_python.py
@@ -1,11 +1,37 @@
 
-import random
+# import random
+#
+# random.seed("seed")
+# print(random.randint(1,1000))
+# print(random.randint(1,1000))
+# print(random.randint(1,1000))
+#
+# print(round(1.444444444444458, 3))
 
-random.seed("seed")
-print(random.randint(1,1000))
-print(random.randint(1,1000))
-print(random.randint(1,1000))
+# индексы и слайсы
 
-print(round(1.444444444444458, 3))
+email = "username@domain.com"
 
+# вывод символа, индексация с начала строки
+print(email[0])
+
+# вывод симовла, индексация с конца строки
+print(email[-2])
+
+# диапазон, выводит 6 символов с 0 индекса
+print(email[0:6])
+
+# диапазон, слайс, вывод 6 символов с 0 индекса
+print(email[:6])
+
+# диапазон, вывести каждый второй символ из подстроки, 10 симоволов с 0 индекса
+print(email[0:10:2])
+#слайс, диапазон с 0 индекса, все символы в строке, с обратным шагом
+print(email[::-1])
+
+#  Оперируем
+
+assert email.endswith("@domain.com")
+
+# форматируем
 
