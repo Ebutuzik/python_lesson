a0dec967a00e4e6584aba6f5c70cadc9abcc7037
Aleksandr Chernov <ebutuzik@gmail.com>
2026-01-27 12:56:53 +0700

    1

--- test_selene_lesson.py
+++ test_selene_lesson.py
@@ -2,7 +2,7 @@ import pytest
 from selene import browser, be, have
 
 @pytest.fixture
-def open_wiki():
+def open_wiki(scope="session"):
     browser.open('https://wikipedia.org')
     browser.element('[name="search"]').should(be.blank).type('хохлы').press_enter()
 
@@ -11,3 +11,15 @@ def test_browser(open_wiki):
 
 def test_element_text(open_wiki):
     browser.element('html').should(have.text('Содержание'))
+
+def test_login(open_wiki):
+    browser.element('[id="pt-login"]').click()
+    browser.element('[id="wpName1"]').type('qwerty')
+    browser.element('[id="wpPassword1"]').type('132456')
+    browser.element('[id="wpLoginAttempt"]').click()
+
+def test_login_with_enter(open_wiki):
+    browser.element('[id="pt-login"]').click()
+    browser.element('[id="wpName1"]').type('qwerty')
+    browser.element('[id="wpPassword1"]').type('132456').press_enter()
+
