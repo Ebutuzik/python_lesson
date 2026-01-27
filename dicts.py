784b1def95044ce5e0dda0db0c30fd301bc5ca8d
Aleksandr Chernov <ebutuzik@gmail.com>
2026-01-27 13:43:07 +0700

    2112

--- dicts.py
+++ dicts.py
@@ -1,5 +1,6 @@
-# Словари
+# # Словари
 from os import name
+from copy import deepcopy
 
 d = {
     "key1": "value1",
@@ -28,8 +29,53 @@ print(users[32])
 users[32] = {"name": "Vano", "age": 57}
 # Функции словарей
 
-users.items()
-users.values()
-users.keys()
+# Получение данных из словаря в виде кортежа
+print(list(users.items()))
+print(list(users.values()))
+print(list(users.keys()))
 
 print(users.get(32, {"name": "default user"}))
+
+# Списки, словари и множества - изменяемые!
+l1 = [1, 2, 3]
+l2 = l1
+l2.append(4)
+
+print(l1)
+print(l2)
+
+print('-------------')
+
+l1 = [1, 2, 3]
+l2 = l1.copy()
+l2.append(4)
+
+print(l1)
+print(l2)
+
+print('-------------')
+
+l1 = [1, 2, 3, [5, 6, 7]]
+l2 = l1.copy()
+l2[-1].append(8)
+l2.append(4)
+
+print(l1)
+print(l2)
+
+print('-------------')
+
+l1 = [1, 2, 3, [5, 6, 7]]
+l2 = deepcopy(l1)
+l2[-1].append(8)
+l2.append(4)
+
+print(l1)
+print(l2)
+
+# Кортежи, frozenset - нет
+
+t1 = (1, 2, 3, 4, 5)
+t2 = t1
+
+frozenset({1, 2, 3, 4, 5, 5, 5})
